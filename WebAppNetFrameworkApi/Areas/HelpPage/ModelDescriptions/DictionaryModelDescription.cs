60b4bacc38931038c1943d87bc88b41faff4c5c6
RodrigoSHL <rodrigo.catalan@tcit.cl>
2024-06-21 11:06:54 -0400

    Agregar archivos de proyecto.

---
+++ WebAppNetFrameworkApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebAppNetFrameworkApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ WebAppNetFrameworkApi/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAppNetFrameworkApi.Controllers
+{
+    public class ProductController : ApiController
+    {
+        // GET api/product
+        public IEnumerable<Product> Get()
+        {
+            return new List<Product>
+            {
+                new Product { Name = "Producto1" },
+                new Product { Name = "Producto2" }
+            };
+        }
+    }
+
+
+    public class Product
+    {
+        public string Name { get; set; }
+    }
+
+
+}
